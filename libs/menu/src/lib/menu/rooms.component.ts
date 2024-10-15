bb680124b4e2ff0b1abe33bda8bc1070f989d9ff
Brancys <brancysb@uninorte.edu.co>
2024-10-15 10:47:43 -0500

    new rooms component

---
+++ libs/menu/src/lib/menu/rooms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-rooms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './rooms.component.html',
+  styleUrl: './rooms.component.css',
+})
+export class RoomsComponent {}
